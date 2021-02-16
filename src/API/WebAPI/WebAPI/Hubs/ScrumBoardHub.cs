1499f936fcdb1021da5a28227aeddf75461cec80
BUDAPEST\Aleksey_Kolesnikov <enlarius@gmail.com>
2021-02-16 12:18:48 +0200

    init backend project with basis

---
+++ src/API/WebAPI/WebAPI/Hubs/ScrumBoardHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Hubs
+{
+    public class ScrumBoardHub : Hub
+    {
+        public async override Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+            await Clients.Caller.SendAsync("Message", "Connected Successfully!");
+        }
+
+        public async Task SubscribeToBoard(Guid boardId)
+        { 
+            
+        }
+    }
+}

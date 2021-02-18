a3d80b887d7dfcd2592708c8657bd6cf2f28b2d1
BUDAPEST\Aleksey_Kolesnikov <enlarius@gmail.com>
2021-02-18 12:28:25 +0200

    BE - readme update, csproj update, controller update.

--- src/API/WebAPI/WebAPI/Controllers/ScrumPokerController.cs
+++ src/API/WebAPI/WebAPI/Controllers/ScrumPokerController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPI.Contracts;
+using WebAPI.Hubs;
 using WebAPI.Infrastructure.Persistence;
 
 namespace WebAPI.Controllers
@@ -13,21 +16,81 @@ namespace WebAPI.Controllers
     public class ScrumPokerController : ControllerBase
     {
 
-        public ScrumPokerController(IScrumRepository scrumRepository)
+        private readonly IScrumRepository scrumRepository;
+        private readonly IHubContext<ScrumBoardHub> hub;
+
+        public ScrumPokerController(IScrumRepository scrumRepository, IHubContext<ScrumBoardHub> hub)
         {
+            this.scrumRepository = scrumRepository;
+            this.hub = hub;
         }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpPost("boards")]
+        public async Task<IActionResult> Post([FromBody] ScrumBoard scrumBoard)
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var boardId = Guid.NewGuid();
+            scrumBoard.Id = boardId;
+
+            var isCreated = await scrumRepository.AddBoard(scrumBoard);
+            if (isCreated)
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                return Ok(boardId);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost("boards/{boardId}")]
+        public async Task<IActionResult> UpdateUsersPoint(Guid boardId)
+        {
+            var isAdded = await scrumRepository.ClearUsersPoint(boardId);
+            await hub.Clients.Group(boardId.ToString())
+                .SendAsync("UsersAdded", await scrumRepository.GetUsersFromBoard(boardId));
+            if (isAdded)
+            {
+                return Ok(isAdded);
+            }
+            return NotFound();
+        }
+
+        [HttpPost("boards/{boardId}/users")]
+        public async Task<IActionResult> AddUser(Guid boardId, User user)
+        {
+            user.Id = Guid.NewGuid();
+            var isAdded = await scrumRepository.AddUserToBoard(boardId, user);
+            await hub.Clients.Group(boardId.ToString())
+                .SendAsync("UsersAdded", await scrumRepository.GetUsersFromBoard(boardId));
+            if (isAdded)
+            {
+                return Ok(user.Id);
+            }
+            return NotFound();
+        }
+
+        [HttpGet("boards/{boardId}/users")]
+        public async Task<IActionResult> GetUsers(Guid boardId)
+        {
+            var users = await scrumRepository.GetUsersFromBoard(boardId);
+
+            return Ok(users);
+        }
+
+        [HttpGet("boards/{boardId}/users/{userId}")]
+        public async Task<IActionResult> GetUser(Guid boardId, Guid userId)
+        {
+            var users = await scrumRepository.GetUsersFromBoard(boardId);
+            var user = users.FirstOrDefault(u => u.Id == userId);
+            return Ok(user);
+        }
+
+        [HttpPut("boards/{boardId}/users")]
+        public async Task<IActionResult> UpdateUser(Guid boardId, User user)
+        {
+            var isUpdated = await scrumRepository.UpdateUserPoint(boardId, user.Id, user.Point);
+            await hub.Clients.Group(boardId.ToString())
+                .SendAsync("UsersAdded", await scrumRepository.GetUsersFromBoard(boardId));
+
+            return Ok(isUpdated);
         }
     }
 }
